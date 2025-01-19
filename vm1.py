d02ce2a4097fbebd37244735678e8ae93f04b7fa
Ayahtullah Centeno <Ayahfahmy2007@gmail.com>
2025-01-19 18:14:07 +0400

    Added features
    initialized self, self.stock used to store cold drink items and snack items

--- vm1.py
+++ vm1.py
@@ -1,3 +1,15 @@
 class BunsVendingMachine:
-    def __init__(self):
-        
+    def __init__(self): # initializing self
+        self.stock = {
+            "Cold Drinks" : { # heading of cold drinks
+                "1" : {"name" : "Strawberry Milk", "price" : 3.00, "stock": 10},
+                "2" : {"name" : "Melon Milk", "price" : 3.00, "stock": 10},
+                "3" : {"name" : "Normal Milk", "price" : 3.00, "stock": 10}
+            },
+            "Snacks" : { # heading for snacks
+                "4" : {"name" : "Strawberry Roll", "price" : 3.00, "stock": 10},
+                "5" : {"name" : "Cream Caramel", "price" : 3.00, "stock": 10},
+                "6" : {"name" : "Spicy Ramen", "price" : 3.00, "stock": 10}
+            },
+        }
+        self.suggestions = {"1" : "4", "2" : "5", "3" : "6"} # suggests pairings for items
